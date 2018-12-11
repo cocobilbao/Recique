308c6fa77e65ced163a670cfa845ef37b00f5f0e
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-12-11 18:48:20 +0100

    Looking good

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect, Route, Link } from "react-router-dom";
 import AuthService from "../../components/auth/AuthService";
 
-import "./Header.scss"
+import "./Header.scss";
 
 export default class Header extends Component {
   constructor() {
@@ -11,23 +11,47 @@ export default class Header extends Component {
   }
 
   render() {
+    const items = document.querySelectorAll(".menu-item");
+
+    function toggleSelect(e) {
+      items.forEach(item => {
+        if (item.classList.contains("selected")) {
+          item.classList.remove("selected");
+        }
+      });
+      e.target.classList.add("selected")
+    }
+
     const navBar = this.props.user ? (
       <div>
-        <Link class="nav-item" to="/">Home</Link> -{" "}
+        <Link class="nav-item" to="/">
+          Home
+        </Link>{" "}
+        -{" "}
         <Link class="nav-item" to="/login" onClick={this.props.logout}>
           Logout
         </Link>
       </div>
     ) : (
       <div>
-         <Link  to="/"><div class="menu-item">Home</div></Link>
-        <Link  to="/signup"><div class="menu-item">Signup</div></Link>{" "}
-        <Link  to="/login"><div class="menu-item">Login</div></Link>
+        <Link to="/">
+          <div class="menu-item" onClick={toggleSelect}>Home</div>
+        </Link>
+        <Link to="/signup">
+          <div class="menu-item" onClick={toggleSelect}>Signup</div>
+        </Link>{" "}
+        <Link to="/login">
+          <div class="menu-item" onClick={toggleSelect}>Login</div>
+        </Link>
       </div>
     );
 
     return (
       <div id="Header">
+      <div class="brand">
+      <h1>hola</h1>
+      </div>
+
         <nav class="menu">{navBar}</nav>
       </div>
     );
